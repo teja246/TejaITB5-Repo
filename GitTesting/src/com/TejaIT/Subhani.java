4e39abff23fcec074fd56d27a899838a54d2f502
admin <admin@DESKTOP-RD2KMH3>
2021-12-13 13:05:06 +0530

    subani from git added lines

--- GitTesting/src/com/TejaIT/Subhani.java
+++ GitTesting/src/com/TejaIT/Subhani.java
@@ -5,6 +5,7 @@ public class Subhani {
 	public static void main(String[] args) {
 	System.out.println("This is Subhani");
 	System.out.println("change made by Teja");
+	System.out.println("subani from git");
 	}
 
 }
