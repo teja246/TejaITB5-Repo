0fd6f9972fe3ae4e86d8bc3ce2d6d0fcafc51eb6
HP <HP@LAPTOP-GU2G0OUG>
2021-12-18 20:35:05 +0530

    kalyan commit

--- GitTesting/src/com/TejaIT/Kalyan.java
+++ GitTesting/src/com/TejaIT/Kalyan.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class Kalyan {
 
 	public static void main(String[] args) {
-		System.out.println("Im Kalyan Kumar.............");
+		System.out.println("Im Kalyan Kumar........................");
 
 	}
 
