986ea02925c00f095bd8343858f919669eeb08a8
Mallikharjunarao K <Mallikharjunarao K@LAPTOP-3VTJ9I85>
2021-12-18 20:48:34 +0530

    malli commits

--- GitTesting/src/com/TejaIT/MalliKajjam.java
+++ GitTesting/src/com/TejaIT/MalliKajjam.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class MalliKajjam {
 
 	public static void main(String[] args) {
-		System.out.println("kajjam..........");
+		System.out.println("kajjam............");
 
 	}
 
