58dc1ed1e75788f1d02812fb2f09877f5cf5153c
Acerr <Acerr@Bashashaik34>
2021-12-18 20:59:38 +0530

    basha changes Merge branch 'master' of https://github.com/teja246/TejaITB2-Repo.git

--- GitTesting/src/com/TejaIT/Basha.java
+++ GitTesting/src/com/TejaIT/Basha.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class Basha {
 
 	public static void main(String[] args) {  
-		System.out.println("this is basha");
+		  System.out.println("this is basha");
       
 	}   
         
