cc71dd7b0f7186f604ef2da8363c848f09e8d302
koti6 <koti6@LAPTOP-J25GCLAU>
2021-12-18 21:16:47 +0530

    kotig commit changes

--- GitTesting/src/com/TejaIT/Kotig.java
+++ GitTesting/src/com/TejaIT/Kotig.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class Kotig {
 
 	public static void main(String[] args) {
-System.out.println("this is    koti.   ...");
+System.out.println("thi     s is    koti.   ...");
 	}
 
 }
