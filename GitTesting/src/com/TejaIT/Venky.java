2787a9c25dbb02c1ca6e168667776db15f940cf2
venky <venky@LAPTOP-BA0U0SS7>
2021-12-18 20:29:47 +0530

    Merge branch 'master' of https://github.com/teja246/TejaITB2-Repo.git
    venky commit changes..........

--- GitTesting/src/com/TejaIT/Venky.java
+++ GitTesting/src/com/TejaIT/Venky.java
@@ -3,8 +3,8 @@ package com.TejaIT;
 public class Venky {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("This is VENKY.........");
 	}
 
 }
