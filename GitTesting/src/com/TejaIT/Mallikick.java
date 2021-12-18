cfd8a03a88b0611e28c19dc82c5a41c131f4c33c
Mallikharjunarao K <Mallikharjunarao K@LAPTOP-3VTJ9I85>
2021-12-18 20:41:19 +0530

    commiting by malli

--- GitTesting/src/com/TejaIT/Mallikick.java
+++ GitTesting/src/com/TejaIT/Mallikick.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class Mallikick {
 
 	public static void main(String[] args) {
-		System.out.println("This is Malli IT...........");
+		System.out.println("This is Malli IT..............");
 
 	}
 
