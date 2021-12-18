ba7a7002df7148bf5790b7c9ec4dd8d262b98a8d
Ashok Pathuri <Ashok Pathuri@LAPTOP-2MQKKU5L>
2021-12-18 20:41:53 +0530

    Ashok commit message

--- GitTesting/src/com/TejaIT/Ashok.java
+++ GitTesting/src/com/TejaIT/Ashok.java
@@ -4,6 +4,6 @@ public class Ashok {
    
 	
 	public static void main(String[] args) {
-	System.out.println("This is teja   It.........");
+	System.out.println("This is teja      It.........");
 }
 }
