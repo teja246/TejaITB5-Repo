3decf5c6c240ba9aa0c1bf56ca74f7d82bd67ba2
PRAVEEN <PRAVEEN@DESKTOP-06UAJ8G>
2021-12-18 08:19:42 +0530

    Praveen commit changes

--- GitTesting/src/com/TejaIT/Praveen.java
+++ GitTesting/src/com/TejaIT/Praveen.java
@@ -3,7 +3,7 @@ package com.TejaIT;
 public class Praveen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("this is praveen.....................");
 
 	}
 
