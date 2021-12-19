f94b445604bb43d0af01a4577378318bd1db1e61
shekar <shekar@LAPTOP-95CB257E>
2021-12-19 10:26:11 +0530

    ShekarloverBoy commit changes

--- GitTesting/src/com/TejaIT/ShekarLoverBoy.java
+++ GitTesting/src/com/TejaIT/ShekarLoverBoy.java
@@ -4,7 +4,7 @@ public class ShekarLoverBoy {
 	
 	public static void main(String[] args) {
 		System.out.println("This is shekar..........");
-		System.out.println("samba");
+		System.out.println("samba siva");
 	}
 
 }
