3bce513e413b6708be34930d075609e141e4a51f
B SHIVA <B SHIVA@LAPTOP-5PILIGU9>
2021-12-27 09:13:52 +0530

    my project is commited

--- GitTesting/src/com/TejaIT/SivaIT.java
+++ GitTesting/src/com/TejaIT/SivaIT.java
@@ -1,10 +1,7 @@
 package com.TejaIT;
 
 public class SivaIT {
-
-	public static void main(String[] args) {
-		System.out.println("this is siva from ktdm.....");
-
-	}
-
+public static void main(String[] args) {
+	System.out.println("this is sivaIT...second commit....");
+}
 }
