e04b6bb031a4b40f9038065b88494e110317637e
vijay <vijay@LAPTOP-L03T5KVB>
2022-03-28 20:26:35 +0530

    vijay chnages

--- GitTesting/src/com/TejaIT/Vijay.java
+++ GitTesting/src/com/TejaIT/Vijay.java
@@ -3,6 +3,6 @@ package com.TejaIT;
 public class Vijay {
 public static void main(String[] args) {
 	System.out.println("This is Vijay");
-	System.out.println("this is....");
+	System.out.println("this is...");
 }
 }
