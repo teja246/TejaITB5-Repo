fb8ed80ddcc8133f39023a032a241ff64e708ee1
HP <HP@LAPTOP-RC0GSGVD>
2022-03-28 20:08:11 +0530

    commit by shanmukha

--- GitTesting/src/com/TejaIT/Shannu.java
+++ GitTesting/src/com/TejaIT/Shannu.java
@@ -3,7 +3,8 @@ package com.TejaIT;
 public class Shannu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is Shanmukha");
+		System.out.println("from Visakhapatnam");
 
 	}
 
