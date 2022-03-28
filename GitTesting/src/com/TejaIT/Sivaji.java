5c87292bb8b4ac236c0b8cc47902cf034188a345
SIVAJI <SIVAJI@LAPTOP-D0KE6MAG>
2022-03-28 20:07:18 +0530

    sivaji changes

---
+++ GitTesting/src/com/TejaIT/Sivaji.java
@@ -0,0 +1,10 @@
+package com.TejaIT;
+
+public class Sivaji {
+	public static void main(String[] args) {
+		System.out.println("This Is Sivaji");
+		System.out.println("from Sivaji Tech");
+		
+	}
+
+}
