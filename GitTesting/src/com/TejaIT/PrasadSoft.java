2e88863117f7d70dc37944b18ef4f26cc993f96f
kasas <kasas@PRASAD>
2022-03-28 20:17:41 +0530

    Merge branch 'master' of https://github.com/teja246/TejaITB2-Repo.git

--- GitTesting/src/com/TejaIT/PrasadSoft.java
+++ GitTesting/src/com/TejaIT/PrasadSoft.java
@@ -4,6 +4,7 @@ public class PrasadSoft {
 	public static void main(String[] args) {
 		System.out.println("this is prasad");
 		System.out.println("from guntur");
+		System.out.println("learn git");
 	}
 
 }
