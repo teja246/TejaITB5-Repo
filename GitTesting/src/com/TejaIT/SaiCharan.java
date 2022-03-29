fd49fdb8725c9c7267bd49d2e28e0a94507365a4
Hello <Hello@DESKTOP-8I3RM2H>
2022-03-29 10:37:25 +0530

    saicharan changes Anusha changes

--- GitTesting/src/com/TejaIT/SaiCharan.java
+++ GitTesting/src/com/TejaIT/SaiCharan.java
@@ -5,7 +5,6 @@ public class SaiCharan {
 	
 	 public static void main(String[] args) {
 		System.out.println("this is sai charan koppula");
-	}
-	
+		System.out.println("life ye oka zindagi aipoindi ra bahi");
+	}	
 }
-
