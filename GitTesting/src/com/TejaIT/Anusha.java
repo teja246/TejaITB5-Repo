dabba5c86ea81435998db857c30057f2974e9bd7
Hello <Hello@DESKTOP-8I3RM2H>
2022-03-29 08:09:27 +0530

    Anusha changes

--- GitTesting/src/com/TejaIT/Anusha.java
+++ GitTesting/src/com/TejaIT/Anusha.java
@@ -3,6 +3,7 @@ package com.TejaIT;
 public class Anusha {
 	public static void main(String[] args) {
 		System.out.println("this is Teja IT....");
+		System.out.println("git clone method");
 		
 	}
 
