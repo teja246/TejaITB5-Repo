28431a89a0320099e95912cb3b2d2a2cfd875eac
vijay <vijay@LAPTOP-L03T5KVB>
2022-03-29 12:51:23 +0530

    vijay changes

--- GitTesting/src/com/TejaIT/Vijay.java
+++ GitTesting/src/com/TejaIT/Vijay.java
@@ -4,6 +4,6 @@ public class Vijay {
 public static void main(String[] args) {
 	System.out.println("This is Vijay");
 	System.out.println("this is...");
-	System.out.println("softwear work......");
+	
 }
 }
