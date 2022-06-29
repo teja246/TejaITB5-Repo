b86a97be590290ea80170b79ecb47cf6abc139df
B R Sarath Kumar <B R Sarath Kumar@RS>
2022-06-29 13:12:46 +0530

    new

--- GitTesting/com/tejait/b4/Sarath.java
+++ GitTesting/com/tejait/b4/Sarath.java
@@ -3,7 +3,7 @@ package com.tejait.b4;
 public class Sarath {
 
 	public static void main(String[] args) {
-		System.out.println("this is sarath");
+		System.out.println("this is sarath....");
 
 	}
 
