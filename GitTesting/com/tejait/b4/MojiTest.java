12a12c787978e6ff1e79b24bb30478567bce4017
ADMIN <ADMIN@DESKTOP-HFRVTJJ>
2022-06-29 13:30:46 +0530

    Merge branch 'master' of https://github.com/teja246/TejaITB2-Repo.git

--- GitTesting/com/tejait/b4/MojiTest.java
+++ GitTesting/com/tejait/b4/MojiTest.java
@@ -2,7 +2,7 @@ package com.tejait.b4;
 
 public class MojiTest {
 	public static void main(String[] args) {
-		System.out.println("This is Moji b4");
+		System.out.println("This is Moji b4...");
 	}
 
 }
