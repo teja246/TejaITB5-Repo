5af6af43aca3adbd413e788342a62056bf0e7f4d
avulagowtham002 <avulagowtham002@gmail.com>
2022-06-29 13:13:58 +0530

    Merge branch 'master' of https://github.com/teja246/TejaITB2-Repo.git

--- GitTesting/SriveniTest.java
+++ GitTesting/SriveniTest.java
@@ -2,7 +2,7 @@ package com.tejait.b4;
 public class SriveniTest {
 	public static void main(String[] args) {
 		System.out.println("This is Sriveni...");
-		System.out.println("again chnaged");
+		System.out.println("again chnaged..");
 	}
 
 }
