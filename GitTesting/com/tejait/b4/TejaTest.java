a95c637cd637ee92ce93bf133714710ca084f80d
teja246 <38396075+teja246@users.noreply.github.com>
2022-10-01 11:22:52 +0530

    Delete TejaTest.java

--- GitTesting/com/tejait/b4/TejaTest.java
+++
@@ -1,9 +0,0 @@
-package com.tejait.b4;
-
-public class TejaTest {
-
-	public static void main(String[] args) {
-	System.out.println("this is Teja tes b4.");
-	System.out.println("Teja Chnages....");
-	}
-}
