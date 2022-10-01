50d54363531bab65b991088671125763f17d1932
User <User@DESKTOP-I7O99V3>
2022-10-01 14:57:47 +0530

    jjj

--- ITC/src/com/Loops/shivaKumar.java
+++ ITC/src/com/Loops/shivaKumar.java
@@ -2,7 +2,7 @@ package com.Loops;
 
 public class shivaKumar {
 	public static void main(String[] args) {
-		System.out.println("shivareddy changes on itc ==");
+		System.out.println("shivareddy  changes on itc ==");
 	}
 
 }
