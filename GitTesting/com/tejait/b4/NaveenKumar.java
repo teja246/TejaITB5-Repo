82345e4a02987f05d7cb20a8b92d7f42552b91f8
teja246 <38396075+teja246@users.noreply.github.com>
2022-10-01 11:23:51 +0530

    Delete NaveenKumar.java

--- GitTesting/com/tejait/b4/NaveenKumar.java
+++
@@ -1,9 +0,0 @@
-package com.tejait.b4;
-
-public class NaveenKumar {
-	
-public static void main(String[] args) {
-	System.out.println("this is NAVEEN KUMAR RAYAPUDI");
-	System.out.println("from teja it batch 4");
-}
-}
