c7f4dc0e2dc7c486d4bdba118785b51402e116eb
teja246 <38396075+teja246@users.noreply.github.com>
2022-10-01 19:09:20 +0530

    chnges done

--- ITC/src/com/Loops/Jyotsna.java
+++ ITC/src/com/Loops/Jyotsna.java
@@ -2,7 +2,7 @@ package com.Loops;
 
 public class Jyotsna {
 	public static void main(String[] args) {
-		System.out.println("this is jyotsna changess");
+		System.out.println("this is jyotsna changess...from TejaIT");
 	}
 
 }
