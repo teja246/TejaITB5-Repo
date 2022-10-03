af8a5eedcb6134e3fb9ae5a4a82600ce87b028a0
teja246 <38396075+teja246@users.noreply.github.com>
2022-10-03 15:48:58 +0530

    Teja Commits

--- ITC/src/com/Loops/Divya.java
+++ ITC/src/com/Loops/Divya.java
@@ -3,6 +3,6 @@ package com.Loops;
 public class Divya {
 	public static void main(String[] args) {
 		
-		System.out.println("this is divya bandaaru from batch-5");
+		System.out.println("this is divya bandaaru from batch-5.....");
 	}
 }
