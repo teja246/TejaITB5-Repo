f83c36ce816d49cb4f85ef16ee2cdc67365529e9
teja246 <38396075+teja246@users.noreply.github.com>
2022-10-05 12:16:51 +0530

    Teja  Changes from test-Work Space

--- ITC/src/com/Loops/ABCTest.java
+++ ITC/src/com/Loops/ABCTest.java
@@ -4,6 +4,7 @@ public class ABCTest {
 
 	public static void main(String[] args) {
 		System.out.println("ABC Test Class from Java8 wrokspace..");
+		System.out.println("ABC Test Class from Test Workspace..");
 	}
 
 }
