d9c13ff18970c7460993de06be228d8c54eec7bf
bussa madhukar <bussa madhukar@LAPTOP-K9H8M134>
2023-01-12 12:12:59 +0530

    Vani changes

--- ITC/src/com/Test/Vani.java
+++ ITC/src/com/Test/Vani.java
@@ -3,7 +3,7 @@ package com.Test;
 public class Vani {
 
 	public static void main(String[] args) {
-          System.out.println(" This is Vani changes");
+          System.out.println(" This is Vani changes..,.");
 
 	}
 
