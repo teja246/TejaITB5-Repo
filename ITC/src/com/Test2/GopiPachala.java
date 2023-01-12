c43d674c71c36b20eee943a197fe82909dedc395
Gopi <Gopi@LAPTOP-GHC7IIIP>
2023-01-12 17:00:41 +0530

    gopi changed

--- ITC/src/com/Test2/GopiPachala.java
+++ ITC/src/com/Test2/GopiPachala.java
@@ -2,6 +2,6 @@ package com.Test2;
 
 public class GopiPachala {
 public static void main(String[] args) {
-	System.out.println("This is Gopi Pachala..........");
+	System.out.println("This is Gopi Pachala............");
 }
 }
