b256748d6f7a39656730a21c1f4cab39e836bd6b
enjam <enjam@enjamoorbhaskar>
2023-01-12 17:04:38 +0530

    Revert "Done latest changes by Bhaskar"
    This reverts commit 925309cafac8fe67798b9ffc472b284e9ddfa704.

--- ITC/src/com/Test2/Bhaskarreddy.java
+++ ITC/src/com/Test2/Bhaskarreddy.java
@@ -3,7 +3,7 @@ package com.Test2;
 public class Bhaskarreddy {
 
 	public static void main(String[] args) {
-System.out.println("This is bhaskar reddy .");
+System.out.println("This is bhaskar reddy");
 
 	}
 
