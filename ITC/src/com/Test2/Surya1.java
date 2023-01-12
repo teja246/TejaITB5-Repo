6054574fff9c2d873863a76d5efee79bc6e96fe9
hi <hi@SURYA>
2023-01-12 23:04:19 +0530

    high

--- ITC/src/com/Test2/Surya1.java
+++ ITC/src/com/Test2/Surya1.java
@@ -5,7 +5,8 @@ public class Surya1 {
 	public static void main(String[] args) {
 		System.out.println("checking surya");
 		System.out.println("checking for stasher");
-		System.out.println("checking update");
+		System.out.println("checking update lo");
+		
 		
 		
 	}
