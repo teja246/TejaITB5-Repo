51887a3b1280878e13f5941b3adfc4a8bc9efba3
teja246 <38396075+teja246@users.noreply.github.com>
2023-01-12 17:50:16 +0530

    Teja changes

--- ITC/src/com/Test2/Teja.java
+++ ITC/src/com/Test2/Teja.java
@@ -5,7 +5,7 @@ public class Teja {
 	public static void main(String[] args) {
 		System.out.println("my changes");
 		System.out.println("again changed");
-		System.out.println("again modified");
+		System.out.println("again modified...");
 	}
 
 }
