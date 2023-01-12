c73d5cea3dd02bc7a647b7253f68a8b8f2d58c4c
ganesh <ganesh@LAPTOP-GT2O586T>
2023-01-12 17:22:06 +0530

    ganesh

--- ITC/src/com/Test2/Ganesh.java
+++ ITC/src/com/Test2/Ganesh.java
@@ -2,7 +2,7 @@ package com.Test2;
 
 public class Ganesh {
 	public static void main(String[] args) {
-		System.out.println("this is ganesh...  ");
+		System.out.println("this is ganesh.....  ");
 		
 		
 	}
