5a81eca656e9fa9fcc4730d3f007ad0e6c483b3f
gopi nayak <gopi nayak@Gopinayak>
2023-01-12 16:23:55 +0530

    Revert "done changes by Gopi nayak"
    This reverts commit e293779f62e6be06b24e6090fa3fe5582beeb54e.

--- ITC/src/com/Test2/GopiNayak.java
+++ ITC/src/com/Test2/GopiNayak.java
@@ -3,6 +3,6 @@ package com.Test2;
 public class GopiNayak {
 	
 	public static void main(String[] args) {
-		System.out.println("my self gopi nayak ");
+		System.out.println("my self gopi nayak");
 	}
 }
