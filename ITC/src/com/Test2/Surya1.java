a736bd2d441b362774619f8c38cde1a9e6f240e1
hi <hi@SURYA>
2023-01-12 23:07:22 +0530

    hello

--- ITC/src/com/Test2/Surya1.java
+++ ITC/src/com/Test2/Surya1.java
@@ -5,7 +5,7 @@ public class Surya1 {
 	public static void main(String[] args) {
 		System.out.println("checking surya");
 		System.out.println("checking for stasher");
-		System.out.println("checking update lo");
+		System.out.println("checking update love ");
 		
 		
 		
