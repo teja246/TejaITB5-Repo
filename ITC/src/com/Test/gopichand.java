191e877fc3017690d9980160855d24cf72134e82
katte <katte@LAPTOP-SRNE88TD>
2023-01-27 16:15:07 +0530

    gopichnad again again changes

--- ITC/src/com/Test/gopichand.java
+++ ITC/src/com/Test/gopichand.java
@@ -2,7 +2,7 @@ package com.Test;
 
 public class gopichand {
 	public static void main(String[] args) {
-		System.out.println("gopichand class....");
+		System.out.println("gopichand class........");
 	}
 
 }
