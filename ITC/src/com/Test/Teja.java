9d4a20cb15cf32f73fd48e6c77a0ff192b905776
hp <hp@LAPTOP-OJ24NHEC>
2023-01-27 16:30:56 +0530

    teja changes

--- ITC/src/com/Test/Teja.java
+++ ITC/src/com/Test/Teja.java
@@ -4,5 +4,6 @@ public class Teja {
 public static void main(String[] args) {
 	System.out.println("This is Teja class");
 	System.out.println("Teja chnaged again...");
+	System.out.println("changed");
 }
 }
