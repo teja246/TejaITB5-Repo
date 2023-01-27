c19e5eefe543f883d798aa9f5eda3ea624b1ef35
91630 <91630@INBOOK_X1_SLIM>
2023-01-27 16:10:20 +0530

    this is MANI changes again

--- ITC/src/com/Test/mani.java
+++ ITC/src/com/Test/mani.java
@@ -3,6 +3,6 @@ package com.Test;
 public class mani {
 
 	public static void main(String[] args) {
-		System.out.println("this is mani from teja IT");
+		System.out.println("this is mani from teja IT.....");
 	}
 }
