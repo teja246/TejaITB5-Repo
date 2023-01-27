6ff104a42d61318e67de13b96af2504ba2185925
Dell <Dell@DESKTOP-QS0CJA6>
2023-01-27 13:47:03 +0530

    hey Gopi changed after Teja

--- ITC/src/com/Test/TejaIT.java
+++ ITC/src/com/Test/TejaIT.java
@@ -1,12 +1,15 @@
 
 package com.Test;
 
+import java.util.Date;
+
 public class TejaIT {
 
 	public static void main(String[] args) {
 		System.out.println("Teja Chnages");
 		System.out.println("changes on 27-01-2023");
 		System.out.println("Teja changed");
+		System.out.println("gopi changed "+new Date());
 	}
 
 }
