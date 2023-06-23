59442940e3b7deb593f854be63628ebff64f9244
DELL <DELL@DESKTOP-4B2QQDV>
2023-06-24 00:36:02 +0530

    vamsi changes made

--- ITC/src/com/batch7/test/Vamsi.java
+++ ITC/src/com/batch7/test/Vamsi.java
@@ -2,7 +2,7 @@ package com.batch7.test;
 
 public class Vamsi {
 	public static void main(String[] args) {
-		System.out.println("vamsi changes");
+		System.out.println("vamsi changes...");
 	}
 
 }
