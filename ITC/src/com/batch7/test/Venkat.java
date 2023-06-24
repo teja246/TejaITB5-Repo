4258e2dfb70d4f210093ea0f0e5597416ce24969
LENOVO <LENOVO@DESKTOP-HVKH6ID>
2023-06-24 12:35:46 +0530

    venkat changes/////

--- ITC/src/com/batch7/test/Venkat.java
+++ ITC/src/com/batch7/test/Venkat.java
@@ -3,6 +3,6 @@ package com.batch7.test;
 public class Venkat {
 
 	public static void main(String[] args) {
-		System.out.println("Venkat Changes........................");
+		System.out.println("Venkat Changes......");
 	}
 }
