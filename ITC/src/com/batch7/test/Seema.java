beb58474b7dfbc8657842ce6c240184be0d205dc
seemanthinisathi <seemasati022@gmail.com>
2023-06-24 12:50:10 +0530

    seema changes - resolving errors

--- ITC/src/com/batch7/test/Seema.java
+++ ITC/src/com/batch7/test/Seema.java
@@ -1,7 +1,8 @@
+package com.batch7.test;
+
 public class Seema {
 
     String s = "dummy";
-
     public static void main(String[] args) {
         System.out.println("random file creation "+ s );
     }
