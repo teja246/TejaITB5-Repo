4ca3883268990176b446bdef03c702e182a20b14
manojyk9951@gmail.com <manojyk9951@gmail.com>
2023-06-24 15:22:18 +0530

    merge manoj

--- ITC/src/com/batch7/test/Manoj.java
+++ ITC/src/com/batch7/test/Manoj.java
@@ -3,6 +3,8 @@ package com.batch7.test;
 public class Manoj {
 	public static void main(String[] args) {
 		System.out.println("manoj changed..");	
+		System.out.println("stash changes....");
+		System.out.println("merge....");
 		
 	}
 	
