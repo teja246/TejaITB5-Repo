d614cbc24efe6685b5848945f56fe602787bf896
P.NAVEEN <P.NAVEEN@Naveen>
2023-06-24 12:33:51 +0530

    bharath changes

--- ITC/src/com/batch7/test/Bharath.java
+++ ITC/src/com/batch7/test/Bharath.java
@@ -3,6 +3,6 @@ package com.batch7.test;
 public class Bharath {
 
 	public static void main(String[] args) {
-		System.out.println("Bharath changes");
+		System.out.println("Bharath    changes");
 	}
 }
