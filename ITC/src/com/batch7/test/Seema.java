37ca6f430230941cfd6f8e79214b295e52cc17c9
seemanthinisathi <seemasati022@gmail.com>
2023-06-24 13:01:28 +0530

    seema stash

--- ITC/src/com/batch7/test/Seema.java
+++ ITC/src/com/batch7/test/Seema.java
@@ -5,5 +5,7 @@ public class Seema {
     //String s = "dummy";
     public static void main(String[] args) {
         System.out.println("random file creation " );
+
+        System.out.println("stashing commit");
     }
 }
