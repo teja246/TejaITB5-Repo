4c0c76211f16fa541178221860b1526091f6a554
manojyk9951@gmail.com <manojyk9951@gmail.com>
2023-06-24 12:23:26 +0530

    manoj changes

--- ITC/src/com/batch7/test/Manoj.java
+++ ITC/src/com/batch7/test/Manoj.java
@@ -2,7 +2,7 @@ package com.batch7.test;
 
 public class Manoj {
 	public static void main(String[] args) {
-		System.out.println("manoj changess");	
+		System.out.println("manoj changes");	
 		
 	}
 	
