dcc26cc79775a462cc36f989b499e5308770288f
avulagowtham002 <avulagowtham002@gmail.com>
2023-06-24 12:28:28 +0530

    gowtham changes

--- ITC/src/com/batch7/test/Gowtham.java
+++ ITC/src/com/batch7/test/Gowtham.java
@@ -2,7 +2,7 @@ package com.batch7.test;
 
 public class Gowtham {
 	public static void main(String[] args) {
-		System.out.println("gowtham changes..");
+		System.out.println("gowtham change..");
 	}
 
 }
