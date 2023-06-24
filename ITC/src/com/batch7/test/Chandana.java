53a24a24098dde2a7e4c735408d3ee8a27bea89e
ADMIN <ADMIN@DESKTOP-AC67NGL>
2023-06-24 16:08:29 +0530

    htdi6luv;i

--- ITC/src/com/batch7/test/Chandana.java
+++ ITC/src/com/batch7/test/Chandana.java
@@ -4,7 +4,7 @@ public class Chandana {
 	
 	public static void main(String[] args) {
 		System.out.println("chandana changes...");
-		System.out.println("hloo..");
+		System.out.println("hai frnds....");
 		
 	}
 
