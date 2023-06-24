e21d523cceecc5411a46275bb9cde582bcf657d3
seemanthinisathi <seemasati022@gmail.com>
2023-06-24 14:05:55 +0530

    seema - updating Keerthi.java

--- ITC/src/com/batch7/test/Keerthi.java
+++ ITC/src/com/batch7/test/Keerthi.java
@@ -2,9 +2,9 @@ package com.batch7.test;
 
 public class Keerthi {
 	public static void main(String[] args) {
+
 		System.out.println("Keerthi changes...");
-		System.out.println("keerthi..........");
-		System.out.println("Hi....... seema......");
-		System.out.println("Hi....... chandu......");
+		System.out.println("hey keerti");
+		System.out.println("wazzup");
 	}
 }
