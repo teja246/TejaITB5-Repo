70607dd15dc5cb30c47c66ae78711045ff5a8525
teja246 <teja.chintapalli246@gmail.com>
2023-12-17 19:17:18 +0530

    HDFCBAN-1: Implement Factory design pattern for downloading statements

---
+++ ITC/src/com/batch8/Simha.java
@@ -0,0 +1,7 @@
+package com.batch8;
+
+public class Simha {
+public static void main(String[] args) {
+	System.out.println("Simhaaa...");
+}
+}
---
+++ ITC/src/com/batch8/Teja.java
@@ -0,0 +1,7 @@
+package com.batch8;
+
+public class Teja {
+public static void main(String[] args) {
+	System.out.println("This file was create by Teja..");
+}
+}
