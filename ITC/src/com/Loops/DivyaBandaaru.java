edd5a13d0edf2b46b2535c5e957d69e9db63b662
teja246 <38396075+teja246@users.noreply.github.com>
2023-12-17 08:00:41 +0530

    Update DivyaBandaaru.java

--- ITC/src/com/Loops/DivyaBandaaru.java
+++ ITC/src/com/Loops/DivyaBandaaru.java
@@ -3,6 +3,6 @@ package com.Loops;
 public class DivyaBandaaru {
 public static void main(String[] args) {
 	
-	System.out.println("this is divya banddaru from batch-5");
+	System.out.println("this is divya banddaru from batch-5..");
 }
 }
