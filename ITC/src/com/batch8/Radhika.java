3cd006ca301ab7b637eef3520fafbc4ccb14c2d2
Anand <Anand@DESKTOP-UANP6HV>
2023-12-17 19:36:54 +0530

    chenges done by radhika

--- ITC/src/com/batch8/Radhika.java
+++ ITC/src/com/batch8/Radhika.java
@@ -2,7 +2,7 @@ package com.batch8;
 
 public class Radhika {
  public static void main(String[] args) {
-	System.out.println("chenges done by Radhika");
+	System.out.println("chenges done by Radhika..");
 }
 
 }
