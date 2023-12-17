4d6ddd14184b7609c25dda46d9cad19844e5c556
teja246 <teja.chintapalli246@gmail.com>
2023-12-17 08:42:44 +0530

    again testing by teja

--- ITC/src/com/batch8/Batch8Test.java
+++ ITC/src/com/batch8/Batch8Test.java
@@ -4,6 +4,7 @@ public class Batch8Test {
 public static void main(String[] args) {
 	System.out.println("Bathc8 Test");
 	
-	System.out.println("Bathc8 Stash Test");
+	System.out.println("Bathc8 Stash Test...");
+	System.out.println("again testing");
 }
 }
