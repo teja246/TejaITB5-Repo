131888b8ee437caa2aef80a42c2640900e50a8c8
rames <rames@venkatesh>
2023-12-17 19:49:22 +0530

    ramesh

--- ITC/src/com/batch8/Ramesh.java
+++ ITC/src/com/batch8/Ramesh.java
@@ -3,6 +3,6 @@ package com.batch8;
 public class Ramesh {
 
 	public static void main(String[] args) {
-		System.out.println("This is created by Ramesh Robo 6.O...");
+		System.out.println("This is created by Ramesh....");
 	}
 }
