4c945955a17f8579062825bad1c70e0eb83beda0
teja246 <teja.chintapalli246@gmail.com>
2023-12-17 09:11:06 +0530

    changes added

---
+++ ITC/src/com/batch8/A.java
@@ -0,0 +1,8 @@
+package com.batch8;
+
+public class A {
+public static void main(String[] args) {
+	System.out.println("added few changes in bathc8Test branch");
+	System.out.println("add new chnages");
+}
+}
