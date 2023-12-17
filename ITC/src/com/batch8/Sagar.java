fe20c3d5456ec6421cf70a65f7e0b5ff84444a4d
teja246 <teja.chintapalli246@gmail.com>
2023-12-17 19:54:41 +0530

    Teja changes in Sagar file

--- ITC/src/com/batch8/Sagar.java
+++ ITC/src/com/batch8/Sagar.java
@@ -3,5 +3,6 @@ package com.batch8;
 public class Sagar {
 public static void main(String[] args) {
 	System.out.println("this is Sagar.....");
+	System.out.println("next changes done by Teja..");
 }
 }
