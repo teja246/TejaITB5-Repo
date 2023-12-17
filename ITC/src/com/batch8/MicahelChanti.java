b6933fe3ab84d3a204d2693bc973ec1a0d4d5cd7
dell <dell@Michael-ThinkPad>
2023-12-17 19:46:45 +0530

    changes done by Michael

---
+++ ITC/src/com/batch8/MicahelChanti.java
@@ -0,0 +1,7 @@
+package com.batch8;
+
+public class MicahelChanti {
+public static void main(String[] args) {
+	System.out.println("This is Michael and Im wishing you all, All the very best...");
+}
+}
