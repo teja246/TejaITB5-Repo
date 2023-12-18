55ca861256476d884e0e96da71015d62a3e1a798
admin <admin@sagar>
2023-12-18 12:14:31 +0530

    changes sagar

--- ITC/src/com/batch8/Sagar.java
+++ ITC/src/com/batch8/Sagar.java
@@ -6,5 +6,8 @@ public static void main(String[] args) {
 	System.out.println("next changes done by Teja..");
 	System.out.println("Again change by sagar....");
 	System.out.println("Again ....change");
+	System.out.println("stash  test1");
+	System.out.println("stash test2");
+	System.out.println("stash test3");
 }
 }
