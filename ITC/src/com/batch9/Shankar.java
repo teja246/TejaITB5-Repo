ecee15114e769f95f6bdcc2fae7be49ced0e9245
shiva <shiva@ShivaYdav>
2024-02-29 15:51:43 +0530

    This Is Shankar changes

--- ITC/src/com/batch9/Shankar.java
+++ ITC/src/com/batch9/Shankar.java
@@ -3,6 +3,8 @@ package com.batch9;
 public class Shankar {
 	public static void main(String[] args) {
 		System.out.println("Shankar personal changes for git ");
+		
+		System.out.println("  Shankar Yadav adding changes");
 	}
 
 }
